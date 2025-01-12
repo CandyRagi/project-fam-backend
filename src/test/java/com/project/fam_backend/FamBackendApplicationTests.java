f55db7a3771765f832c863860d6e429a71ed04b2
Candy <tiwariansh935@gmail.com>
2025-01-12 02:53:32 -0800

    setup

---
+++ src/test/java/com/project/fam_backend/FamBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.fam_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
