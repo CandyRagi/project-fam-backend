67a9934269633a621957bd9abab7bf509cea8871
Candy <tiwariansh935@gmail.com>
2025-01-12 03:09:47 -0800

    copied all user functions from previous project files

--- src/main/java/com/project/fam_backend/Objects/LoginRequest.java
+++ src/main/java/com/project/fam_backend/Objects/LoginRequest.java
@@ -1,5 +1,24 @@
 package com.project.fam_backend.Objects;
 
 public class LoginRequest {
+    private String userName;
+    private String passphrase;
 
+
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassphrase() {
+        return passphrase;
+    }
+
+    public void setPassphrase(String passphrase) {
+        this.passphrase = passphrase;
+    }
 }
--- src/main/java/com/project/fam_backend/Repos/UserRepo.java
+++ src/main/java/com/project/fam_backend/Repos/UserRepo.java
@@ -1,5 +1,15 @@
 package com.project.fam_backend.Repos;
 
-public class UserRepo {
+import com.project.fam_backend.Objects.*;
+import java.util.Optional;
 
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepo extends MongoRepository<User,String>{
+
+    @Query("{'name':?0}")
+    Optional<User> findByUserName(String name);
+    
 }
+
